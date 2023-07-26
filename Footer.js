8738326c998b3970204dcdc6f7ad886eeb16d887
vishnupcse <76236924+Vishnu6056@users.noreply.github.com>
2023-07-26 11:21:00 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = ({length}) => {
+    // const year = new Date();
+  return (
+    <footer>
+        {/* Copyright &copy; {year.getFullYear()} */}
+
+        {length} available tasks are here
+    </footer>
+  )
+}
+
+export default Footer;
+
